a8767a2d34a8c7095d36558cbd65402b036eb853
firealex2 <alex.rotileanu@yahoo.com>
2016-08-25 19:28:14 +0300

    aye
    first upload

---
+++ Unravel/Assets/Tiled2Unity/Scripts/Item.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+    public bool is_on = false;
+    public float damage = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(is_on)
+        {
+            transform.position = Player_Movement.player_position;
+        }
+	}
+}
---
+++ Unravel/Assets/Tiled2Unity/Scripts/SmoothFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour
+{
+
+    //public Transform player;
+    // Use this for initialization
+    private Transform player = Player_Movement.player_position;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        player = Player_Movement.player_position;
+        transform.position = new Vector3(player.position.x, player.position.y, -10);
+    }
+}
